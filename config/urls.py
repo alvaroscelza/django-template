cac860cfb7bde0f9dddea4e4bf908d21ed71f108
Alvaro Andrés Rodríguez Scelza <alvaroscelza@gmail.com>
2021-06-01 21:08:12 -0300

    template urls.py file

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = '<<Project Name>>'
+admin.site.index_title = _('<<Project description>>')
+admin.site.site_title = '<<Project Name>>'
