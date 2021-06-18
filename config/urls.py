068b04b32e1bb67e153e964183e4bb718383daac
Alvaro Andrés Rodríguez Scelza <alvaroscelza@gmail.com>
2021-06-18 15:55:51 -0300

    Changed URLs
    -to use settings app name and description
    -to include index template.

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,15 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.utils.translation import gettext_lazy as _
+from django.urls import include, path
+from django.views.generic import TemplateView
 
-urlpatterns = i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False)
+index_data = {'app_name': settings.APP_NAME, 'app_description': settings.APP_DESCRIPTION}
+index_controller = TemplateView.as_view(template_name="index.html", extra_context=index_data)
+urlpatterns = i18n_patterns(path('', index_controller), prefix_default_language=False)
+urlpatterns += i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-admin.site.site_header = '<<Project Name>>'
-admin.site.index_title = _('<<Project description>>')
-admin.site.site_title = '<<Project Name>>'
+admin.site.site_header = settings.APP_NAME
+admin.site.index_title = settings.APP_DESCRIPTION
+admin.site.site_title = settings.APP_NAME
