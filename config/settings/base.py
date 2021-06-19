f9a573b0d16c5785408e30599f2539261a60c9dc
Loaderon <alvaroscelza@gmail.com>
2021-06-19 20:09:04 -0300

    Added tag for app name.

--- config/settings/base.py
+++ config/settings/base.py
@@ -51,6 +51,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'generic_helper': 'templatetags.generic_helper',
+            },
         },
     },
 ]
--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-index_data = {'app_name': settings.APP_NAME, 'app_description': settings.APP_DESCRIPTION}
-index_controller = TemplateView.as_view(template_name="index.html", extra_context=index_data)
-urlpatterns = i18n_patterns(path('', index_controller), prefix_default_language=False)
+urlpatterns = i18n_patterns(path('', TemplateView.as_view(template_name="index.html")), prefix_default_language=False)
 urlpatterns += i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False)
 urlpatterns += i18n_patterns(path('core/', include('applications.core.urls')), prefix_default_language=False)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
