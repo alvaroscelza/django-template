d2d7f9e2e60020d13aa3822a672604abc58c1007
Loaderon <alvaroscelza@gmail.com>
2022-01-03 22:00:26 -0300

    deleted amazon configurations from prod file.

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,16 +9,3 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_HOST = "s3.us-east-2.amazonaws.com"
-AWS_S3_REGION_NAME = "us-east-2"
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-options = DATABASES['default'].get('OPTIONS', {})
-options.pop('sslmode', None)
