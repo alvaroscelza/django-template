54da464d0eb6545ff5e3eaf5467dface9eac8448
Alvaro Scelza <alvaroscelza@gmail.com>
2022-08-27 23:29:38 -0300

    added missing imports

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 
 # region Admin configuration
