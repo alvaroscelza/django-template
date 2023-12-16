7a399eff55a4b206fef9c1df20b92fbeda7632ad
Alvaro Scelza <alvaroscelza@gmail.com>
2023-12-16 11:47:43 +0100

    changed views for controllers

--- applications/tenants/urls.py
+++ applications/tenants/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
-from applications.tenants.views.organizations_view import OrganizationsView
+from applications.tenants.controllers.organizations_view import OrganizationsView
 
 router = DefaultRouter()
 router.register(r'organizations', OrganizationsView, basename='organizations')
